c510e579ba703336b83c0eb36692197e6f32e8b5
Leandro ribeiro gomes <ledielsong9@gmail.com>
2024-08-01 10:44:18 -0300

    Initial commit

---
+++ Python_lord.py
@@ -0,0 +1,52 @@
+from io import IncrementalNewlineDecoder
+from optparse import IndentedHelpFormatter
+from socket import J1939_NLA_PAD, J1939_NO_ADDR, J1939_NO_NAME, J1939_NO_PGN, J1939_PGN_ADDRESS_CLAIMED, J1939_PGN_ADDRESS_COMMANDED, J1939_PGN_MAX, J1939_PGN_REQUEST
+from tkinter import PROJECTING
+from xml.dom import xmlbuilder
+from xml.etree.ElementTree import fromstring
+from xml.sax.saxutils import XMLGenerator
+
+"CLASS-PROJECTING" 
+("Classtesto"): (class)XMLGenerator; # type: ignore
+
+
+property ("Meu jogos");
+
+#("Estali meu jogo nesse programa");(J1939_NO_NAME);
+
+#("Estali meu jogo nesse programa");(J1939_NO_ADDR);
+
+#("x = 1 Altura - lagura do quadrado");
+
+#("y = 1 Altura - Lagura do quadrado");
+
+#(J1939_PGN_MAX);   (J1939_PGN_ADDRESS_COMMANDED);
+
+#(x = 1 , y = 1);("Altura _ Lagura x = 1 j1939_PGN_REQUEST") * ("Altura _ Lagura y = 1 J1939_PGN_ADDRESS_CLAIMED");
+
+#("10 = 10 * (20 = 20) ");
+
+#("0.1< 2.0");
+
+#(' 0.1, 2.0   10 = 10 * 20 = 20');
+
+#("Do meu jogo");
+
+#("Desenho") ("x =1 Altura - Lagura do quadrado")("y = 1 Altura - Lagura do quadrado");
+# 1/1_2/3 sobre x=1\y=10;
+#("figura do desenho") ("x =10 Altura - Lagura do quadrado")("y = 10 Altura - Lagura do quadrado");
+#20/20_10/1.25 y=10\x=50;
+#("Jogabilidade do desenho") ("x =1 Altura - Lagura do quadrado")("y = 1 Altura - Lagura do quadrado");
+# ("J1939_NO_PGN"),("J1939_NLA_PAD")<("j1939_PGN-MAX"),("j1939_PGN_ADDRESS_COMMANDED");
+#("Sensibilidade do desenho") ("x =100% Altura - Lagura do quadrado")("y = 100% Altura - Lagura do quadrado");
+#x=1<y=2.(altura*lagura);
+#tamanho do quadrado 2.00=2.00;
+#("Desenho") ("x =1 Altura - Lagura do quadrao")("y = 1 Altura - Lagura do quadrado");("figura do desenho") ("x =10 Altura - Lagura do quadrado")("y = 10 Altura - Lagura do quadrado");
+#("jogobilidade do desenho") ("x =1 Altura - Lagura do quadrado")("y =1 Altura - Lagura do quadrado")("sensinilidade do desenho") ("x =100% Altura - lagura do quadrado")("y =100% Altura - Lagura do quadrado");
+# figura ,jogabilidade ,sensibilidade;
+#x=1<y=2.(altura*lagura);
+#tamanho do quadrado 2.00=2.00;
+#figura do jogo;
+#jogabilidade do jogo;
+
+#jogabilidade do jogo para window;
